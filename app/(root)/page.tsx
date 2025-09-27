605539cbf8c041ff1ef95084f9264e83ea266cba
ninazmul <nazmulsaw@gmail.com>
2025-09-27 20:04:03 +0600

    root page on under development

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
-
 export default async function Home() {
-
   return (
-    <>
-      <p>Under Development</p>
-    </>
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-gray-800 mb-4">🚧 Under Development</h1>
+        <p className="text-lg text-gray-600">
+          We’re working hard to bring something amazing here. Stay tuned!
+        </p>
+      </div>
+    </div>
   );
 }
