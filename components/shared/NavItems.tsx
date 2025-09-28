fa38eff7354f640003687ff33a927504a193fb79
ninazmul <nazmulsaw@gmail.com>
2025-09-28 07:02:47 +0600

    nav items updated

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -21,7 +21,7 @@ const NavItems = ({ onItemSelected }: NavItemsProps) => {
             key={link.route}
             className={`${
               isActive && "lg:text-white lg:bg-white/20 rounded-md"
-            } flex-center p-medium-20 whitespace-nowrap px-2 py-1`}
+            } flex-center p-medium-16 whitespace-nowrap px-2 py-1`}
           >
             <Link href={link.route} onClick={onItemSelected}>
               {link.label}
