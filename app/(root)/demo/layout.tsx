a94d8948a523513d60dec555faa06d432266aad3
zeehan <uzeehan@gmail.com>
2026-08-02 14:49:14 +1000

    suffah, rename the layout to demo

--- app/(root)/demo/layout.tsx
+++ app/(root)/demo/layout.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/sharedNew2026/Header";
 import ScrollHeaderWrapper from "@/components/shared/ScrollHeaderWrapper";
 import { Toaster } from "react-hot-toast";
 
-export default function RootLayout({
+export default function DemoLayout({
   children,
 }: {
   children: React.ReactNode;
