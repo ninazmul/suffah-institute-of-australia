aef0ebf126411ca014f8a314b1cc8ea5cf8d738f
zeehan <uzeehan@gmail.com>
2026-08-03 21:59:07 +1000

    suffah, change layout code

--- app/(demo)/layout.tsx
+++ app/(demo)/layout.tsx
@@ -1,23 +1,44 @@
-import Footer from "@/components/sharedNew/Footer";
-import Header from "@/components/sharedNew/Header";
-import ScrollHeaderWrapper from "@/components/sharedNew/ScrollHeaderWrapper";
-// @ts-expect-error - CSS import type declaration is missing in this workspace
-import "../globalsNew.css";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { isAdmin } from "@/lib/actions/admin.actions";
+import { getUserEmailById } from "@/lib/actions/user.actions";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import AdminSidebar from "./components/AdminSidebar";
+import { cookies } from "next/headers";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 import { Toaster } from "react-hot-toast";
 
-export default function DemoLayout({
+export default async function DemoLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const cookieStore = await cookies();
+  const defaultOpen = cookieStore.get("sidebar:state")?.value === "true";
+
+  const { sessionClaims } = await auth();
+
+  const userId = sessionClaims?.userId as string;
+  const email = await getUserEmailById(userId);
+  const adminStatus = await isAdmin(email);
+
+  if (!adminStatus) {
+    redirect("/");
+  }
+  
   return (
-    <div className="flex h-screen flex-col">
+    <SidebarProvider defaultOpen={defaultOpen}>
+      <AdminSidebar />
       <Toaster />
-      <ScrollHeaderWrapper>
-        <Header />
-      </ScrollHeaderWrapper>
-      <main className="flex-1 pt-28 __main">{children}</main>
-      <Footer />
-    </div>
+      <main className="flex-1 h-screen mx-auto overflow-y-auto">
+        <div className="flex justify-between items-center p-4 w-full border-b text-white bg-gradient-to-r from-primary-900 to-primary-500">
+          <SidebarTrigger />
+          <SignedIn>
+            <UserButton afterSwitchSessionUrl="/" />
+          </SignedIn>
+        </div>
+        {children}
+      </main>
+    </SidebarProvider>
   );
 }
