2b4a1a4e9a0b6afbdbeb8b984b50bbffbf3e7727
zeehan <uzeehan@gmail.com>
2026-08-09 05:14:56 +1000

    suffah, roll back nextConfig

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.md$/,
-      type: 'asset/source', // Treats .md files as raw text string assets
-    });
-    return config;
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        port: "",
+      },
+    ],
   },
 };
 
