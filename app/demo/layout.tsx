328b6b76ad6cf917430f0700b62b6b81a4800e83
zeehan <uzeehan@gmail.com>
2026-08-09 04:57:44 +1000

    suffah, fix build errors

--- app/demo/layout.tsx
+++ app/demo/layout.tsx
@@ -4,7 +4,6 @@ import { isAdmin } from "@/lib/actions/admin.actions";
 import { getUserEmailById } from "@/lib/actions/user.actions";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import AdminSidebar from "./components/AdminSidebar";
 import { cookies } from "next/headers";
 import { SignedIn, UserButton } from "@clerk/nextjs";
 import { Toaster } from "react-hot-toast";
@@ -39,7 +38,6 @@ export default async function DemoLayout({
   
   return (
     <SidebarProvider defaultOpen={defaultOpen}>
-      <AdminSidebar />
       <Toaster />
       <main className="demo-layout">
         <div className="flex justify-between items-center p-4 w-full border-b text-white bg-gradient-to-r from-primary-900 to-primary-500">
--- app/demo/services/page.tsx
+++ app/demo/services/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-import "./globalsNew2026.css";
 
 export default function ServicesPage() {
   const services = [
