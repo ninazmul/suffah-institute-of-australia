d136b5fc8d2f0d59e3feefa76c45195a40be095c
zeehan <uzeehan@gmail.com>
2026-08-09 05:32:17 +1000

    suffah, add process env key

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,7 +80,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <html lang="en">
         <body className={poppins.variable}>
           {children}
