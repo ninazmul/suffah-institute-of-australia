6c6865461425e48ec99b7ff6efd1120171c379b0
zeehan <uzeehan@gmail.com>
2026-08-12 13:40:45 +1000

    suffah, change header layout

--- components/sharedNew/Header.tsx
+++ components/sharedNew/Header.tsx
@@ -18,18 +18,9 @@ export default async function Header() {
   const email = await getUserEmailById(userId);
   const adminStatus = await isAdmin(email);
   return (
-    <header className="header w-full text-white bg-gradient-to-r from-primary-900 to-primary-500">
+    <header className="header w-full text-white">
       <div className="wrapper flex items-center justify-between gap-2">
         <Link href="/" className="flex items-center gap-2">
-          <Image
-            src="/assets/images/logo.png"
-            width={100}
-            height={100}
-            className="w-10 h-10 md:w-8 md:h-8 lg:w-12 lg:h-12"
-            priority
-            quality={100}
-            alt="Suffah Institute of Australia logo"
-          />{" "}
           <h1 className="md:text-3xl lg:text-5xl font-serif font-bold text-white hidden md:flex">
             Suffah Institute of Australia
           </h1>
@@ -42,13 +33,49 @@ export default async function Header() {
             </h3>
           </div>
         </Link>
-
+        <div className="flex items-center gap-3">
+          <SignedIn>
+            {adminStatus && (
+              <Button
+                asChild
+                variant={"outline"}
+                size={"sm"}
+                className="rounded-full border-white"
+              >
+                <Link href="/dashboard">
+                  <Shield />
+                  Admin
+                </Link>
+              </Button>
+            )}
+            <UserButton afterSwitchSessionUrl="/" />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild variant={"outline"} className="rounded-full">
+              <Link href="/sign-in">
+                <LogIn />
+                <span className="">Login</span>
+              </Link>
+            </Button>
+          </SignedOut>
+        </div>
         <Donation />
       </div>
 
       <hr className=" border-t border-white" />
 
-      <div className="wrapper flex items-center justify-between">
+      <div className="wrapper flex items-center justify-between gap-2">
+        <Link href="/" className="flex items-center gap-2">
+          <Image
+            src="/assets/images/logo.png"
+            width={100}
+            height={100}
+            className="w-10 h-10 md:w-8 md:h-8 lg:w-12 lg:h-12"
+            priority
+            quality={100}
+            alt="Suffah Institute of Australia logo"
+          />{" "}
+        </Link>
         <div>
           <nav className="lg:flex-between hidden w-full max-w-xs">
             <NavItems />
@@ -56,33 +83,10 @@ export default async function Header() {
           <MobileNav />
         </div>
 
+      </div>
+
+      <div className="wrapper flex items-center justify-between">
         <div className="flex items-center gap-x-2">
-          <div className="flex items-center gap-3">
-            <SignedIn>
-              {adminStatus && (
-                <Button
-                  asChild
-                  variant={"outline"}
-                  size={"sm"}
-                  className="rounded-full border-white"
-                >
-                  <Link href="/dashboard">
-                    <Shield />
-                    Admin
-                  </Link>
-                </Button>
-              )}
-              <UserButton afterSwitchSessionUrl="/" />
-            </SignedIn>
-            <SignedOut>
-              <Button asChild variant={"outline"} className="rounded-full">
-                <Link href="/sign-in">
-                  <LogIn />
-                  <span className="">Login</span>
-                </Link>
-              </Button>
-            </SignedOut>
-          </div>
         </div>
       </div>
     </header>
