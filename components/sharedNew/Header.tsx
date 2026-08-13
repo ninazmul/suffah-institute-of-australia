4d29d62a95dd3968cd617482896aae329f09e088
zeehan <uzeehan@gmail.com>
2026-08-13 20:23:49 +1000

    suffah, add navbar capsule css class

--- components/sharedNew/Header.tsx
+++ components/sharedNew/Header.tsx
@@ -19,7 +19,7 @@ export default async function Header() {
   const adminStatus = await isAdmin(email);
   return (
     <header className="header w-full text-white">
-      <div className="wrapper flex items-center">
+      <div className="navbar--capsule wrapper flex items-center">
         <Link href="/" className="flex items-center gap-2">
           <h1 className="brand-title font-bold text-white hidden md:flex">
             Suffah Institute of Australia
