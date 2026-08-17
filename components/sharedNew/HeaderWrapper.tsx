581a6176119299524d6880e87c250e20a73b9af6
zeehan <uzeehan@gmail.com>
2026-08-17 11:16:55 +1000

    suffah, add init animation on scroll

--- components/sharedNew/HeaderWrapper.tsx
+++ components/sharedNew/HeaderWrapper.tsx
@@ -1,6 +1,30 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+
 export default function HeaderWrapper({children}: {children: React.ReactNode}) {
+
+    // const { hasInitAnimation, setHasInitAnimation } = useState(false);
+
+    useEffect(() => {
+        // setHasInitAnimation(true);
+
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                document.querySelector('header')?.classList.add('init-animation');
+            } else {
+                document.querySelector('header')?.classList.contains('init-animation') && document.querySelector('header')?.classList.remove('init-animation');
+            }
+        };
+        
+        window.addEventListener('scroll', handleScroll);
+
+        handleScroll(); // Call it once to set the initial state based on the current scroll position
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, []);
     return (
         <header className="header w-full text-white">
             {children}
