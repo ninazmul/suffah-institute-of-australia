224746f9d1e1ca2407ffc02a61b7c5b6ef7f6e36
zeehan <uzeehan@gmail.com>
2026-08-19 13:34:52 +1000

    suffah, remove div

--- components/sharedNew/ScrollHeaderWrapper.tsx
+++ components/sharedNew/ScrollHeaderWrapper.tsx
@@ -29,10 +29,7 @@ export default function ScrollHeaderWrapper({
 
   return (
     <div
-      className="header-container"
-    >
-    <div
-      className={`fixed top-0 left-0 w-full transition-transform duration-300 z-[40] ${
+      className={`header-container fixed top-0 left-0 w-full transition-transform duration-300 z-[40] ${
         showHeader
           ? "transform translate-y-0 shadow-black/20 shadow-lg"
           : "transform -translate-y-full"
