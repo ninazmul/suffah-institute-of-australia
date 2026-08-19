28bcd691a7721f785dbc4f1ac270f754d22da0f8
zeehan <uzeehan@gmail.com>
2026-08-19 12:26:35 +1000

    suffah, remove async from contact page

--- app/demo/contact/page.tsx
+++ app/demo/contact/page.tsx
@@ -1,27 +1,26 @@
 import { ContactUs } from "@/components/shared/ContactUs";
 import Link from "next/link";
 
-export default async function ContactPage() {
+export default function ContactPage() {
   return (
-      <div>
-        <section className="wrapper flex flex-col gap-4">
-          <h2 className="h2-bold py-4">Contact Us</h2>
-          <ContactUs />
-        </section>
-        <section className="section-links">
-          <p className="p-regular-20 md:p-regular-24 py-4">
-            if you want to ask a question about the religion Islam please navigate
-            to{" "}
-            <Link
-              href={"/bookings"}
-              className="hover:underline text-primary-500 font-semibold"
-            >
-              ‘Ask the Sheikh’
-            </Link>{" "}
-            page.
-          </p>
-        </section>
-
-      </div>
+    <div>
+      <section className="wrapper flex flex-col gap-4">
+        <h2 className="h2-bold py-4">Contact Us</h2>
+        <ContactUs />
+      </section>
+      <section className="section-links">
+        <p className="p-regular-20 md:p-regular-24 py-4">
+          if you want to ask a question about the religion Islam please navigate
+          to{" "}
+          <Link
+            href={"/bookings"}
+            className="hover:underline text-primary-500 font-semibold"
+          >
+            &lsquo;Ask the Sheikh&rsquo;
+          </Link>{" "}
+          page.
+        </p>
+      </section>
+    </div>
   );
 }
