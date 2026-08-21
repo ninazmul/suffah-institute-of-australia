af91f7f909bea05ceb9ee66775593f0cf08479b2
zeehan <uzeehan@gmail.com>
2026-08-21 15:49:38 +1000

    suffah, move banner into header wrapper

--- app/demo/contact/page.tsx
+++ app/demo/contact/page.tsx
@@ -1,22 +1,26 @@
 import { ContactUs } from "@/components/sharedNew/ContactUs";
 import Link from "next/link";
 import Image from "next/image";
+import HeaderWrapper from "@/components/sharedNew/HeaderWrapper";
 
 export default function ContactPage() {
   return (
     <div>
-      <section className="wrapper flex flex-col gap-4">
-        <div className="absolute inset-0">
-          <Image
-            src="/assets/images/banner-2.png"
-            alt="SIA Purpose Background"
-            fill
-            priority
-            className="object-cover opacity-60"
-          />
-        </div>
-        <h2 className="h2-bold py-4">Contact Us</h2>
-      </section>
+      <HeaderWrapper>
+        <section className="wrapper flex flex-col gap-4">
+          <div className="absolute inset-0">
+            <Image
+              src="/assets/images/banner-3.jpg"
+              alt="SIA Purpose Background"
+              fill
+              priority
+              className="object-cover opacity-60"
+            />
+          </div>
+          <h2 className="h2-bold py-4">Contact Us</h2>
+        </section>
+      </HeaderWrapper>
+
       <ContactUs />
       <section className="section-links">
         <p className="p-regular-20 md:p-regular-24 py-4">
